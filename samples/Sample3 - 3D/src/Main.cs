b34639cd3e87484aed62219a56c3bc60db8c749c
shabbywu <shabbywu@qq.com>
2024-10-06 20:43:33 +0800

    update sample3

--- samples/Sample3 - 3D/src/Main.cs
+++ samples/Sample3 - 3D/src/Main.cs
@@ -3,11 +3,21 @@ using System;
 namespace ThreeD;
 public partial class Main : Node3D {
     private Gui _gui;
+    private RigidBody3D _box;
+    private RigidBody3D _sphere;
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() {
+    public override void _Ready()
+    {
+        _sphere = GetNode<RigidBody3D>("Sphere");
+        _box = GetNode<RigidBody3D>("Box");
+        
         _gui = GetNode<Gui>("Gui");
-        _gui.OnDomReady += () => { GD.Print("on dom ready"); };
+        _gui.OnDomReady += () =>
+        {
+            _sphere.Freeze = false;
+            _box.Freeze = false;
+        };
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
